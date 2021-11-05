645ecc6e83b06c29e88667d2f3905409b82003b8
Claudia Font <claudiafontmarques@gmail.com>
2021-11-05 19:11:50 +0100

    added rules in eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
   },
 };
