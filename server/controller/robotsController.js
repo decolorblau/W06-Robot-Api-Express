e7fb1bb617d9fad4ca7fb7a195720de6d0e7e7d8
Claudia Font <claudiafontmarques@gmail.com>
2021-11-07 13:13:54 +0100

    added getRobotById controller

--- server/controller/robotsController.js
+++ server/controller/robotsController.js
@@ -5,6 +5,24 @@ const getRobots = async (req, res) => {
   res.json(robots);
 };
 
+const getRobotById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const searchedRobot = await Robot.findById(id);
+    if (searchedRobot) {
+      res.json(searchedRobot);
+    } else {
+      const error = new Error("Pet not found");
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+};
+
 module.exports = {
   getRobots,
+  getRobotById,
 };
