09207aa7ab1c2a8a182b5c0b0bfd404492dae8bd
Claudia Font <claudiafontmarques@gmail.com>
2021-11-07 14:10:49 +0100

    added getById route

--- server/routes/robotsRoutes.js
+++ server/routes/robotsRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getRobots } = require("../controller/robotsController");
+const { getRobots, getRobotById } = require("../controller/robotsController");
 
 const router = express.Router();
 
 router.get("/", getRobots);
+router.get("/:idRobots", getRobotById);
 
 module.exports = router;
